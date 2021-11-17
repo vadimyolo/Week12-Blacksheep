a53fd563c169963b9580e349cef1baafeae875b1
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:16:41 +0200

    sheep

---
+++ RandomQuotes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace RandomQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string directoryPath = @"C:\Users\opilane\Samples";
+            string fileName = "quotes.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+            Random rnd = new Random();
+            Console.WriteLine(dataFromFile[0]);
+            Console.WriteLine(dataFromFile[rnd.Next(dataFromFile.Length)]);
+        }
+    }
+}
---
+++ Seasons/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Seasons
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string winterfile = @"C:\Users\opilane\samples\Winter.txt";
+            string autumnfile = @"C:\Users\opilane\samples\Autumn.txt";
+
+            string[] winterData = File.ReadAllLines(autumnfile);
+            string[] autumnData = File.ReadAllLines(winterfile);
+
+            File.WriteAllLines(autumnfile, autumnData);
+            File.WriteAllLines(winterfile, winterData);
+            
+
+            
+        }
+    }
+}
